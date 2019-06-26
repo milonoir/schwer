f95e2f3dca84d37e5ed17e54e4041db4e648767e
Milan Boleradszki <milan.boleradszki@multiplay.co.uk>
2019-06-26 12:29:12 +0100

    Add CPU monitoring and update README

--- README.md
+++ README.md
@@ -1,10 +1,51 @@
-# schwer
+# Schwer
 
-A little HTTP server with built-in web frontend that can produce cpu/mem load.
+_adjective_ | /ʃveːɐ/ | _meaning_: heavy
 
-### TODO
+A single binary application with built-in web front-end that can produce cpu/mem load.
 
-- Flesh out CPU load
-- Memory load
-- Resource monitor
-- Make it work on Windows
+
+## Installation
+
+1. Clone this repo
+2. Change working directory to your local cloned repo
+3. `$ go generate . && go build .`
+
+
+## Usage
+
+Once you built the application, just run it. Schwer, by default, binds to port `9999`, but you
+can specify a different port (between 1024-65535) if you like by using the `-port` flag:
+
+`$ ./schwer -port 19999`
+
+
+### Web
+
+While Schwer is running, you can open the web front-end in your browser by visiting `localhost:<port>`.
+
+You should see something like this:
+
+![Schwer index page](img/schwer_index.png)
+
+The amount of CPU load percentage Schwer can produce can be specified by typing a number between
+0 - 100 into the little input field and hitting enter or clicking the `[Update]` button.
+
+
+### API
+
+You can also use Schwer via its HTTP API.
+
+| Endpoint | Method | Params | Response code |  Description |
+| -------- | ------ | ------ | ------------- |  ----------- |
+| `/cpu`   | `GET`  | `-`    | 200 OK        | Returns an array of CPU utilisation levels per core (e.g. `[49, 34, 50, 32]` in case of a machine with 4 cores). |
+| `/cpu`   | `POST` | `pct` - load level % (0-100) | 202 Accepted<br>400 Bad Request | Sets the load level for Schwer to produce. |
+
+
+## TODO
+
+- [ ] Flesh out CPU load
+- [x] CPU load monitor
+- [ ] Memory load
+- [ ] Memory load monitor
+- [ ] Make it work on Windows
--- web/script.js
+++ web/script.js
@@ -18,15 +18,79 @@ $(document).ready(function() {
     });
   });
 
+  const meterWidth = 50;
+  const meterHeight = 102;
+  const gap = 30;
+
+  function draw(data) {
+    var canvas = document.getElementById("cpu-monitor");
+    if (canvas.getContext) {
+      var ctx = canvas.getContext("2d");
+
+      // Clear canvas.
+      ctx.fillStyle = "#ffffff";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Handle error message.
+      if (typeof(data) === "string") {
+        ctx.fillStyle = "#000000";
+        ctx.font = "24px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(data, canvas.width / 2, canvas.height / 2);
+        return;
+      }
+
+      // Determine coordinates of the first CPU meter.
+      var startX = (canvas.width - (data.length * meterWidth + (data.length - 1) * gap)) / 2;
+      var startY = (canvas.height - meterHeight) / 2;
+
+      for (i = 0; i < data.length; i++) {
+        var x = startX + i * (meterWidth + gap);
+
+        // Meter border. Less code than ctx.rect().
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(x, startY, meterWidth, meterHeight);
+
+        // Fill in the entire meter with the color matching the load level.
+        if (data[i] >= 90) {
+          ctx.fillStyle = "#d53600"; // Red above 90%.
+        } else if (data[i] >= 70) {
+          ctx.fillStyle = "#ffbf00"; // Amber above 70%.
+        } else {
+          ctx.fillStyle = "#60a917"; // Green below 70%.
+        }
+        ctx.fillRect(x + 1, startY + 1, meterWidth - 2, meterHeight - 2);
+
+        // Paint the 100% - level part white.
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(x + 1, startY + 1, meterWidth - 2, meterHeight - 2 - data[i]);
+
+        // Add textual representation of load level. This gives the impression of the meters
+        // filling up from the bottom.
+        ctx.fillStyle = "#000000";
+        ctx.font = "16px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(data[i].toString(), x + meterWidth / 2, startY + meterHeight / 2);
+      }
+    }
+  };
+
+  const errServerDown = "Unable to connect to Schwer server.";
+
   // This functions updates CPU utilisation levels.
   function cpuPoll() {
-    var status = $("#cpu-monitor");
+    var fb = $("#cpu-monitor-fallback");
     $.ajax({
       type: "GET",
       url: "/cpu",
       dataType: "json",
       success: function(data) {
-        status.text(data);
+        fb.text(data);
+        draw(data);
+      },
+      error: function() {
+        fb.text(errServerDown);
+        draw(errServerDown);
       }
     });
   };
